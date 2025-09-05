505ed2ea9f68f75a45f9a0f903a03a7c80631af1
Rui Ribeiro <ruyrybeyro@gmail.com>
2025-09-05 08:58:18 +0100

    org at lower RAM for older 16KB models

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# create out.tap from z80typeZX.bas
 zmakebas -a10 -nZ80TEST z80typeZX.bas
+# create z80typeZX.tap for all ZX Spectrum 
+# and clones
 pasmo --tap z80typeZX.asm z80typeZX1.tap
+# z80typeZX.tap = out.tap + z80typeZX1.tap 
 cat out.tap z80typeZX1.tap > z80typeZX.tap
-rm out.tap z80typeZX1.tap
+rm out.tap z80typeZX1.tap z80typeZX.lst 2> /dev/null
